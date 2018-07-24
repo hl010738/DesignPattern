81302f5f1b8bf653229f43692b4e0138eaf9fcc4
talent.he <talent.he@bamboonetworks.com>
2018-07-24 14:50:28 +0800

    Strategy 策略模式

--- src/Strategy/BubbleSortStrategy.kt
+++ src/Strategy/BubbleSortStrategy.kt
@@ -4,9 +4,10 @@ class BubbleSortStrategy: Strategy{
     override fun sort(list: MutableList<Int>) {
         println("Bubble Sort Strategy invoked")
 
-        var outIndex: Int = 0
-        var inIndex: Int = 0
-        var temp: Int = 0
+        var outIndex = 0
+        var inIndex: Int
+        var temp: Int
+
         while (outIndex < list.size) {
             inIndex = outIndex + 1
             while (inIndex < list.size) {
